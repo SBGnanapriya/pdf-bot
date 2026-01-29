2fb23e5832decffc13593fc502c63c141edeabb4
SBGnanapriya <sbgnanapriya2006@gmail.com>
2026-01-29 13:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pypdf import PdfReader
 
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import HuggingFacePipeline
